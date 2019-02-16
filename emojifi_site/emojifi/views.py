9a6f1392c047b1ad2e97a8bda335677c3b575f70
btj12 <btj12@case.edu>
2019-02-15 20:41:02 -0500

    Working html template and endpoint

--- emojifi_site/emojifi/views.py
+++ emojifi_site/emojifi/views.py
@@ -1,6 +1,27 @@
+import json
+import itertools
+import emoji
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import response
+from django.http import HttpRequest
 
 
-def index(request):
-    return HttpResponse("Hello, world!")
+def index(request: HttpRequest):
+    return render(request, 'emojifi/main.html')
+
+
+@csrf_exempt
+def clapifi(request: HttpRequest):
+    if request.method == 'POST':
+        print(str(request.body))
+        original_text: str = json.loads(request.body)['text']
+        word_list = original_text.split(' ')
+        return HttpResponse(
+            json.dumps({
+                'text': ' clap '.join(word_list)
+            }),
+            content_type='application/json',
+        )
+    return HttpResponse(status=403)
