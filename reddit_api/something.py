9e16f73152e0f784c8cac7a840f7d0feda37f05b
ThomasLerner <tsl26@case.edu>
2019-02-16 22:20:23 -0500

    fixed reddit bot, added pointer to server

--- reddit_api/something.py
+++ reddit_api/something.py
@@ -1,16 +1,22 @@
 import praw
 import requests
+from time import sleep
+import json
 
 reddit = praw.Reddit('bot')
 
-for item in reddit.inbox.unread(limit=None):
-	subject = item.subject.lower()
-	if subject == 'username mention' and isinstance(item, praw.models.Comment):
-		parent = item.parent()
-		payload = {'text': parent.body(),
-				   'type': 'search'}
-		requests.post('127.0.0.1', data=None, json=payload)
-		r = requests.get('127.0.0.1')
-		payload = r.json()
-		reply = item.reply(payload['text'] + '/n /n ^I am a bot. I was made by /u/HotBrass.')
-		item.mark_read()
+while True:
+	for item in reddit.inbox.unread(limit=None):
+		subject = item.subject.lower()
+		if subject == 'username mention' and isinstance(item, praw.models.Comment):
+			parent = item.parent()
+			payload = {'text': parent.body,
+					   'type': 'search'}
+			r = requests.post('http://3.18.14.202/emojifi', data=json.dumps(payload))
+			finaltext = r.json()
+			reply = item.reply(finaltext['text'] + '\n\n ^I am a bot. I was made by /u/HotBrass.')
+			item.mark_read()
+	sleep(10)
+
+
+# 3.18.14.202:80
