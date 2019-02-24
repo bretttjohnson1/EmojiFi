3ff75004e32f2b1ddeeaedab29011d0c0b5fdfc2
btj12 <btj12@case.edu>
2019-02-23 19:04:15 -0500

    Fix transform

--- emoji_search/emojisearch/util/word_transform.py
+++ emoji_search/emojisearch/util/word_transform.py
@@ -1,8 +1,10 @@
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
 
 
 def lematize_words(word_list):
     return [WordNetLemmatizer().lemmatize(word, pos='v') for word in word_list]
 
+
 def synonyms(word):
-    return [synset.name().split('.')[0] for synset in wordnet.synsets(word)]
+    return {synset.name().split('.')[0] for synset in wordnet.synsets(word)}
