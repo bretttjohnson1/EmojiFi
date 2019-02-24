87ad60f4caf5473dba0846640dd1cb17bdb5c866
btj12 <btj12@case.edu>
2019-02-24 00:15:46 -0500

    Fix stop word caps issue and add new stopwords

--- emoji_search/emojisearch/util/stopwords.py
+++ emoji_search/emojisearch/util/stopwords.py
@@ -1,8 +1,12 @@
 from functools import lru_cache
 from nltk.corpus import stopwords
+from emojisearch.paths import STOPWORD_PATH
 
 
 @lru_cache(maxsize=1)
 def stop_words():
     """ Reads the stopwords file and loads it into a set """
-    return set(stopwords.words('english'))
+    stop_word_set = set()
+    with open(STOPWORD_PATH, 'r') as stop_words_file:
+        stop_word_set = stop_word_set.union(set(stop_words_file.readlines()))
+    return stop_word_set.union(set(stopwords.words('english')))
